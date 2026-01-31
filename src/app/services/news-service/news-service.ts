c17c357c379d5cbecc1739d541dbacbcb26123a2
FatAndreasbot <guva.andrej.01@gmail.com>
2026-01-31 13:57:56 +0700

    finished news service

--- src/app/services/news-service/news-service.ts
+++ src/app/services/news-service/news-service.ts
@@ -1,8 +1,25 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { map, Observable } from 'rxjs';
+import { INewsArticle } from '../../interfaces/inews-article';
 
 @Injectable({
   providedIn: 'root',
 })
 export class NewsService {
+private endpointURL = "http://localhost:8000/api/news"
+
+  constructor(
+    private http:HttpClient
+  ) { }
+
+  getAllNews():Observable<INewsArticle[]>{
+    return this.http.get<INewsArticle[]>(this.endpointURL)
+  }
   
+  getById(id:number):Observable<INewsArticle[]>{
+    return this.http.get<INewsArticle[]>(this.endpointURL).pipe(
+      map(arr => arr.filter(article => article.id == id))
+    )
+  }
 }
