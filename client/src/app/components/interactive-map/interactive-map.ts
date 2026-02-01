90a89c6e63af69cf88fc70578fcd012433312203
Андрей Гува (Andrew Guva) <guva.andrej.01@gmail.com>
2026-02-01 16:23:27 +0700

    map is showing

--- client/src/app/components/interactive-map/interactive-map.ts
+++ client/src/app/components/interactive-map/interactive-map.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, signal, WritableSignal } from '@angular/core';
+import * as L from 'leaflet'
+import { IPosition } from '../../interfaces/iposition';
+import { PositionService } from '../../services/position-service/position-service';
 
 @Component({
   selector: 'app-interactive-map',
@@ -6,6 +9,29 @@ import { Component } from '@angular/core';
   templateUrl: './interactive-map.html',
   styleUrl: './interactive-map.css',
 })
-export class InteractiveMap {
+export class InteractiveMap implements AfterViewInit {
+  private map!: L.Map
+  points: WritableSignal<IPosition[]> = signal([])
 
+  constructor(
+    private positionService: PositionService
+  ) { }
+
+  ngOnInit() { 
+  }
+
+  ngAfterViewInit(): void {
+    this.map = L.map('map', {
+      center:[56, 92.9],
+      zoom:3
+    })
+
+    const tiles = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      maxZoom: 18,
+      minZoom: 12,
+      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    })
+
+    tiles.addTo(this.map)
+  }
 }
