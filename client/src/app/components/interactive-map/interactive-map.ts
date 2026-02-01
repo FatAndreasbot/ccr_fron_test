6d9600306c759ba4805fd91e025707e13ba7d7fd
Андрей Гува (Andrew Guva) <guva.andrej.01@gmail.com>
2026-02-01 17:27:59 +0700

    showing all postions with names as popups

--- client/src/app/components/interactive-map/interactive-map.ts
+++ client/src/app/components/interactive-map/interactive-map.ts
@@ -11,24 +11,28 @@ import { PositionService } from '../../services/position-service/position-servic
 })
 export class InteractiveMap implements AfterViewInit {
   private map!: L.Map
-  points: WritableSignal<IPosition[]> = signal([])
 
   constructor(
     private positionService: PositionService
   ) { }
 
   ngOnInit() { 
+    this.positionService.getAllPoints().subscribe((positions) => {
+      positions.forEach((pos, i, arr) => {
+        L.marker([pos.latitude, pos.longitude])
+          .bindPopup(`<p>${pos.name}</p>`)
+          .addTo(this.map)
+      })
+    })
   }
 
   ngAfterViewInit(): void {
     this.map = L.map('map', {
       center:[56, 92.9],
-      zoom:3
+      zoom:12
     })
 
     const tiles = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      maxZoom: 18,
-      minZoom: 12,
       attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
     })
 
